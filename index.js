c287fcd694ea63ba28577bdfcf00ebf314d8df0f
SafeFly <ast850328@gmail.com>
2017-06-29 19:13:44 +0800

    add API for checking door's status

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const Http = require('http');
+const BodyParser = require('body-parser');
 const rpio = require('rpio');
 const ENV = require('./env.js');
 
 let app = express();
 
 // set lock's pin. Default is lock
-rpio.open(ENV.PIN, rpio.OUTPUT, rpio.HIGH);
+rpio.open(ENV.PIN, rpio.OUTPUT, rpio.LOW);
 
-app.use(bodyParser.urlencoded());
-app.use( bodyParser.json() );
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use( BodyParser.json() );
 
 // switch lock status
 app.post('/switch', (request, response) => {
   const status = rpio.read(ENV.PIN);
   let res = 'yet';
- 
   let token = request.body.token;
   let isToken = false;
- 
   for (keyIndex in ENV.TOKENS) {
+    console.log(ENV.TOKENS[keyIndex]);
     if (token === ENV.TOKENS[keyIndex]) {
       isToken = true;
       break;
@@ -28,7 +28,7 @@ app.post('/switch', (request, response) => {
   
   if (!isToken) {
     res = 'error';
-    return response.status(404).send(JSON.stringify(res));
+    return response.status(400).send(JSON.stringify(res));
   }
 
   if (status === rpio.HIGH) {
@@ -45,6 +45,21 @@ app.post('/switch', (request, response) => {
   });
   response.status(200).send(JSON.stringify(res));
 
+});
+
+// get status
+app.get('/status', (req, res) => {
+  const status = rpio.read(ENV.PIN);
+  let msg = null;
+  if (status) {
+    msg = "MOLi is close now Φ౪Φ."
+  } else {
+    msg = "MOLi is open now (=^-ω-^=)."
+  }
+  return res.status(200).json({
+    status: status,
+    message: msg
+  });
 });
 
 app.listen(ENV.PORT, () => {
